553e9f98d2c2fa9b25d0c1c1202c5f207cd1bd24
신용준 <4rest528@gmail.com>
2026-09-03 22:59:22 +0900

    Update ShaderW0.hlsl

--- WEEK2TEAM3/ShaderW0.hlsl
+++ WEEK2TEAM3/ShaderW0.hlsl
@@ -1,47 +1,34 @@
 cbuffer constants : register(b0) // FConstants
 {
-    float3 Offset;
-    float Rotation;
-    float3 Scale;
-    float AspectRatio;
+	float3 Offset;
+	float Pad1;
+	float3 Scale;
+	float Pad2;
 }
 
 struct VS_INPUT
 {
-    float4 position : POSITION;
-    float4 color : COLOR;
+	float4 position : POSITION;
+	float4 color : COLOR;
 };
 
 struct PS_INPUT
 {
-    float4 position : SV_POSITION;
-    float4 color : COLOR;
+	float4 position : SV_POSITION;
+	float4 color : COLOR;
 };
 
 PS_INPUT mainVS(VS_INPUT input) // Vertex Shader
 {
-    PS_INPUT output;
+	PS_INPUT output;
     
-    // Scale
-    float2 scaled = input.position.xy * Scale.xy;
+	output.position = input.position * float4(Scale, 1.0) + float4(Offset, 0);
+	output.color = input.color;
     
-    // Rotate
-    float cosTheta = cos(Rotation);
-    float sinTheta = sin(Rotation);
-    float2 rotated = float2(scaled.x * cosTheta - scaled.y * sinTheta, scaled.x * sinTheta + scaled.y * cosTheta);
-    
-    // Offset
-    float2 translated = rotated + Offset.xy;
-    
-    translated.x /= AspectRatio;
-    
-    output.position = float4(translated, input.position.z, 1.0);
-    output.color = input.color;
-    
-    return output;
+	return output;
 }
 
 float4 mainPS(PS_INPUT input) : SV_TARGET // Pixel Shader
 {
-    return input.color;
+	return input.color;
 }
